399f75d784a461e49a23d12c5100a135a20eb783
Freddie Rigby <rigbyfreddie685@gmail.com>
2021-11-11 08:43:19 +0000

    Puts each query solution

--- app/helpers/gets_film_data.rb
+++ app/helpers/gets_film_data.rb
@@ -13,7 +13,7 @@ module GetsFilmData
     "
     sparql = SPARQL::Client.new("http://dbpedia.org/sparql")
     result = sparql.query(query)
-    puts result
+    result.each {|solution| puts solution.to_h}
   end
 
   def get_actors_from_film(film)
